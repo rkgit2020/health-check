4648ac1c7f6fc3927c4c66f7a7787f2220984113
Ramakrishna Nethi <ramakrishna.nethi@oracle.com>
2023-08-05 17:24:22 +0530

    added CPU constrained function

--- all_checks.py
+++ all_checks.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import sys
 import socket
-
+import psutil
 
 def check_reboot():
 	"""Returns True if the computer has a pending reboot. """
@@ -25,6 +25,11 @@ def check_root_full():
 	"""Returns Ture if the root partition is full, False otherwise. """
 	return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+def check_cpu_constrained():
+	"""Returns True if the cpu is having too much usage, False otherwise."""
+	return psutil.cpu_percent(1) > 75
+
+
 def check_no_network():
 	"""Returns True if it fails to resolve google's URL, False otherwise"""
 	try:
@@ -38,7 +43,8 @@ def main():
 	checks = [
 		(check_reboot, "Pending Reboot"),
 		(check_root_full, "Root partition full"),
-		(check_no_network, "No working Network")
+		(check_no_network, "No working Network"),
+		(check_cpu_constrained, "CPU load too high"),
 	]
 	everything_ok = True
 	for check,msg in checks:
